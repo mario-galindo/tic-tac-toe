0307d2ec3cbe11dee71e40317f5f1c74c36d6d7d
Mario-Galindo <mgalindo9410@gmail.com>
2020-02-29 22:21:00 -0600

    se agrega constructor y estado al componente

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    render() {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      value = null,
+    };
+  }
+  
+  render() {
       return (
         <button className="square" onClick={() => alert('click')}>
           {this.props.value}
